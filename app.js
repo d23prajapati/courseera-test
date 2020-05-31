c7c22b94daa89f30e9e0b493f6161a2bc703fe02
unknown <darshan@courseera-something.com>
2020-05-31 11:18:37 +0530

    Assignment 2

--- app.js
+++ app.js
@@ -1,19 +1,75 @@
 (function(){
   'use strict';
-  angular.module('LunchCheck',[])
-  .controller('LunchCheckController',LunchCheckController);
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope){
-    $scope.item="";
-    $scope.message="";
-    $scope.splitItem = function(){
-      var arr = $scope.item.split(",");
+  angular.module('ShoppingListCheckOff',[])
+  .controller('ToBuyController',ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
-      if($scope.item=="")
-              {$scope.message="Please enter data first!";}
-      else if(arr.length <= 3)
-              {$scope.message="Enjoy!";}
-      else {$scope.message="Too much!";}
-    };
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+  var buy = this;
+  buy.items = ShoppingListCheckOffService.getToBuyList();
+
+  buy.removeItem = function(itemIndex,itemName,itemQuantity){
+    ShoppingListCheckOffService.addToBoughtList(itemName,itemQuantity);
+    ShoppingListCheckOffService.removeFromToBuyList(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+  var alreadyBought = this;
+  alreadyBought.items = ShoppingListCheckOffService.getBoughtList();
+}
+  function ShoppingListCheckOffService(){
+    var service = this;
+
+      var shoppingList = [
+        {
+          name: "Cookies",
+          quantity: "10"
+        },
+        {
+          name: "Chips",
+          quantity: "5"
+        },
+        {
+          name: "Biscuits",
+          quantity: "6"
+        },
+        {
+          name: "Cold drinks",
+          quantity: "15"
+        },
+        {
+          name: "Noodles",
+          quantity: "20"
+        },
+      ];
+
+      var toBuyList = shoppingList;
+      var bought = [];
+
+      service.getToBuyList = function(){
+        return toBuyList;
+      };
+
+      service.removeFromToBuyList = function(itemIndex){
+        toBuyList.splice(itemIndex,1);
+      };
+
+      service.getBoughtList = function(){
+        return bought;
+      };
+
+      service.addToBoughtList = function(itemName,itemQuantity){
+        var item = {
+          name: itemName,
+          quantity: itemQuantity
+        };
+        bought.push(item);
+      };
+
+
   }
 })();
